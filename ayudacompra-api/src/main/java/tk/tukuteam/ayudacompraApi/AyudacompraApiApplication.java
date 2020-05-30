40bcb3249a5999afceb836e510db9ed1ff9b0a18
Leonard Petit-Breuilh <nik.leonard@gmail.com>
2020-05-30 10:53:07 -0400

    Configuración Inicial de proyectos

---
+++ ayudacompra-api/src/main/java/tk/tukuteam/ayudacompraApi/AyudacompraApiApplication.java
@@ -0,0 +1,13 @@
+package tk.tukuteam.ayudacompraApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyudacompraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AyudacompraApiApplication.class, args);
+	}
+
+}
---
+++ ayudacompra-api/src/test/java/tk/tukuteam/ayudacompraApi/AyudacompraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tk.tukuteam.ayudacompraApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AyudacompraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
