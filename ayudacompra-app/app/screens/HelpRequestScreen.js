7f8c54043b91be5cbab63dff0811b356f2787abb
Fabiola Orellana <fabiola.orellana.g@gmail.com>
2020-05-31 12:03:39 -0400

    wip: avance HelpRequest

--- ayudacompra-app/app/screens/HelpRequestScreen.js
+++ ayudacompra-app/app/screens/HelpRequestScreen.js
@@ -1,28 +1,57 @@
 import "react-native-gesture-handler";
 import React from "react";
-import { StyleSheet,View, ScrollView } from "react-native";
+import { StyleSheet, View, ScrollView } from "react-native";
 import { Button, Avatar, Text, Input, Divider } from "react-native-elements";
 
 const HelpRequestScreen = ({ navigation }) => {
-    return (
-        <ScrollView>
-            <Text h4>Mis datos</Text>
-            <View style={{flexDirection:'row',justifyContent:'flex-start',alignItems:"center", margin:10}}>
-                <Avatar icon={{name: 'user', type: 'font-awesome'}}
-                overlayContainerStyle={{backgroundColor: 'blue'}} size="small" rounded activeOpacity={0.7}/>
-                <Text style={{margin:10}}>Usuario de prueba 1</Text>
-            </View>
-            <Divider style={{ backgroundColor: 'grey' }} />
-            <Text h4>Entregar en:</Text>
-            <Input placeholder="Calle, n°, depto/block" label="Dirección"/>
-            <Input label="Comuna / Ciudad"/>
-            <Input placeholder="Tipo o nombre del comercio" label="Comercio" />
-            <Button title="Lista de compras" onPress={() => navigation.navigate("CreateProductListScreen")} />
-            <Text>
-
-            </Text>
-        </ScrollView>
-    );
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title} h4>
+        Mis datos
+      </Text>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "flex-start",
+          alignItems: "center",
+          margin: 10,
+        }}
+      >
+        <Avatar
+          icon={{ name: "user", type: "font-awesome" }}
+          overlayContainerStyle={{ backgroundColor: "blue" }}
+          size="small"
+          rounded
+          activeOpacity={0.7}
+        />
+        <Text style={{ margin: 10 }}>Usuario de prueba 1</Text>
+      </View>
+      <Divider style={{ backgroundColor: "grey" }} />
+      <Text h4>Entregar en:</Text>
+      <Input placeholder="Calle, n°, depto/block" label="Dirección" />
+      <Input label="Comuna / Ciudad" />
+      <Input placeholder="Tipo o nombre del comercio" label="Comercio" />
+      <Button
+        title="Lista de compras"
+        onPress={() => navigation.navigate("CreateProductListScreen")}
+      />
+      <Text></Text>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    padding: 20,
+  },
+  title: {
+    textAlign: "center",
+    color: "#34495e",
+    fontWeight: "bold",
+  },
+});
 
 export default HelpRequestScreen;
